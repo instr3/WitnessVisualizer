69be0da5bca5a376eecde0256afc06ecb748870d
instr3 <instr3@163.com>
2021-02-05 13:54:39 +0800

    Added missing files

---
+++ PuzzleGraph/Decorators/SymmetryPuzzleDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PuzzleGraph.Decorators
+{
+    public class SymmetryPuzzleDecorator: TransformableDecorator, IFaceDecorable, IEdgeDecorable, INodeDecorable
+    {
+        [LocalizedDisplayName("SecondLineColor")]
+        [XmlElement(Type = typeof(XmlColor))]
+        public Color SecondLineColor { get; set; } = Color.Aqua;
+
+        [LocalizedDisplayName("IsRotational")]
+        public bool IsRotational { get; set; } = false;
+    }
+}
---
+++ PuzzleGraph/TransformableDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleGraph
+{
+    public class TransformableDecorator : Decorator
+    {
+        [LocalizedDisplayName("DeltaX")]
+        public double DeltaX { get; set; } = 0.0;
+        [LocalizedDisplayName("DeltaY")]
+        public double DeltaY { get; set; } = 0.0;
+        [LocalizedDisplayName("Angle")]
+        public double Angle { get; set; } = 0.0;
+        [LocalizedDisplayName("ExtraScale")]
+        public double ExtraScale { get; set; } = 1.0;
+    }
+}
