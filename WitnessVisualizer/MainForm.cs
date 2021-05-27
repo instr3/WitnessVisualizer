0cbb4dde3f3ad7dee8bb5cda289628f50e26d105
instr3 <instr3@163.com>
2021-05-27 17:07:27 +0800

    Fixed file opening issue

--- WitnessVisualizer/MainForm.cs
+++ WitnessVisualizer/MainForm.cs
@@ -465,7 +465,7 @@ namespace WitnessVisualizer
 
         private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            saveInfoFileDialog.InitialDirectory = Path.Combine(Application.StartupPath, "Puzzles");
+            // saveInfoFileDialog.InitialDirectory = Path.Combine(Application.StartupPath, "Puzzles");
             if (editView != null)
             {
                 if (saveInfoFileDialog.ShowDialog() == DialogResult.Cancel)
--- WitnessVisualizer/Program.cs
+++ WitnessVisualizer/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +18,8 @@ namespace WitnessVisualizer
         [STAThread]
         static void Main(string[] args)
         {
+            string exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Directory.SetCurrentDirectory(exeDir);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm(args));
